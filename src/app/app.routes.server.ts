1584fd05ec4ce41ffc32d3ff3540b30d34b667d4
Oscar Duvan Garcia Ochoa <oscar.garcia.ochoa@unillanos.edu.co>
2026-05-21 14:06:16 -0500

    fix(deploy): migrate production setup to cloudflare pages

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,4 +1,4 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
+import { RenderMode, type ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   { path: '', renderMode: RenderMode.Prerender },
@@ -8,5 +8,5 @@ export const serverRoutes: ServerRoute[] = [
   { path: 'preguntas-frecuentes', renderMode: RenderMode.Prerender },
   { path: 'nosotros', renderMode: RenderMode.Prerender },
   { path: 'contacto', renderMode: RenderMode.Prerender },
-  { path: '**', renderMode: RenderMode.Server },
+  { path: '**', renderMode: RenderMode.Client },
 ];
